8d33fdd84ccef01ea0449767a860ea12f2d04be3
Jalal-Amourgha <myproo100@gmail.com>
2023-09-03 19:43:45 +0100

    small changes

--- js.js
+++ js.js
@@ -1,12 +1,14 @@
 let screen = document.querySelector(".screen");
 let btns = document.querySelectorAll(".button");
 let string = "";
+let result = 0;
 
 btns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         if (e.target.innerHTML === "=") {
             string = eval(string);
             screen.value = string;
+            string = '';
         } else if (e.target.innerHTML === "AC") {
             string = "";
             screen.value = string;
@@ -19,3 +21,10 @@ btns.forEach((btn) => {
         }
     })
 });
+
+screen.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        e.keyCode = 187
+    }
+    result += e;
+})
